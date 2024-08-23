cf6e2365a1d015582754c091d92805c354cd2908
Gabriel Pires de Barros <gdbarros95@gmail.com>
2024-08-23 21:39:10 +0000

    software finalziado

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int option;
+
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Exibir saudação");
+            System.out.println("2. Calcular soma de dois números");
+            System.out.println("3. Sair");
+            System.out.print("Escolha uma opção: ");
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    Utils.displayGreeting();
+                    break;
+                case 2:
+                    System.out.print("Digite o primeiro número: ");
+                    int num1 = scanner.nextInt();
+                    System.out.print("Digite o segundo número: ");
+                    int num2 = scanner.nextInt();
+                    int sum = Utils.calculateSum(num1, num2);
+                    System.out.println("A soma é: " + sum);
+                    break;
+                case 3:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+                    break;
+            }
+        } while (option != 3);
+
+        scanner.close();
+    }
+}
---
+++ src/Utils.java
@@ -0,0 +1,9 @@
+public class Utils {
+    public static void displayGreeting() {
+        System.out.println("Olá! Bem-vindo ao programa.");
+    }
+
+    public static int calculateSum(int a, int b) {
+        return a + b;
+    }
+}
